ea1d38e3daf964e00e9df6721978aa79a98dc0e6
thomaspsl <thomas.peissel.tp@gmail.com>
2025-01-07 09:00:06 +0100

    Initial Commit

---
+++ splashy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  splashy
+//
+//  Created by Thomas on 07/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
